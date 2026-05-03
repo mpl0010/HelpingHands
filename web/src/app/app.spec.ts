904937e58c76289ac58f78de808dea2ccff1fbeb
Matthew Lloyd <mplloyd14@gmail.com>
2026-05-03 17:53:50 -0500

    Add tests for router

--- web/src/app/app.spec.ts
+++ web/src/app/app.spec.ts
@@ -1,11 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { LandingPageComponent } from './pages/landing/landing-page.component';
+import { NotFoundPageComponent } from './pages/not-found/not-found-page.component';
 
 describe('App', () => {
+    let harness: RouterTestingHarness;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [App]
+            imports: [App],
+            providers: [
+                provideRouter([
+                    { path: '', component: LandingPageComponent },
+                    { path: '**', component: NotFoundPageComponent },
+                ]),
+            ]
         }).compileComponents();
+
+        harness = await RouterTestingHarness.create();
     });
 
     it('should create the app', () => {
@@ -26,5 +40,15 @@ describe('App', () => {
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
         expect(compiled.querySelector('app-footer')).toBeTruthy();
+    });
+
+    it('should display landing page component for default route', async () => {
+        await harness.navigateByUrl('');
+        expect(harness.routeNativeElement?.tagName.toLowerCase()).toBe('app-landing-page');
+    });
+
+    it('should display not found page component for not found route', async () => {
+        await harness.navigateByUrl('/not-found-test');
+        expect(harness.routeNativeElement?.tagName.toLowerCase()).toBe('app-not-found-page');
     });
 });
